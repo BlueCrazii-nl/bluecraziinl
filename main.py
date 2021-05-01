e02a00816fd911f61718d82ac15933ca5997ada7
Ellie Ang <git@angm.xyz>
2021-05-01 17:42:27 +0200

    Abstract template into base and child

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ import yaml
 with open('data.yml') as f:
     data = yaml.safe_load(f)
 
-subs = jinja2.Environment(
-    loader=jinja2.FileSystemLoader('./')
-).get_template('templates/index.html').render(data=data)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates'))
+index = env.get_template('index.html').render(data=data)
 
 with open('index.html', 'w') as f:
-    f.write(subs)
+    f.write(index)
