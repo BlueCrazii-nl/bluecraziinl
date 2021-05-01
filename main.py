e487aa54c6e21dd9555472263c31ed06036de5c2
Ellie Ang <git@angm.xyz>
2021-05-01 19:33:32 +0200

    Fix inconsistent formatting and refactor templating

--- main.py
+++ main.py
@@ -4,42 +4,51 @@ import os
 import jinja2
 import yaml
 
+# Load input data & jinja
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates'))
 with open('data.yml') as f:
     data = yaml.safe_load(f)
 
+# Create a directory, ignore if it already exists
 def mkdir(name):
     try:
         os.mkdir(name)
     except:
         pass
-
+# Create all required directories
 mkdir('watch/')
 mkdir('sets/')
 for set in data['livesets_menu']:
-    mkdir('watch/' + set['link'])
+    mkdir(f'watch/{set["link"]}')
 for set in data['livesets_cards']:
-    mkdir('watch/' + set['link'])
+    mkdir(f'watch/{set["link"]}')
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates'))
+# Get template with given name
+def tem(name):
+    return env.get_template(f'{name}.html')
 
+# Write a rendered template to the given path.
 def write(dat, location):
-    with open(location, 'w') as f:
+    with open(f'{location}.html', 'w') as f:
         f.write(dat)
 
-write(env.get_template('index.html').render(data=data), 'index.html')
+# Create and write index and stream pages
+write(tem('index').render(data=data), 'index')
 for stream in data['streams']:
     stream['resolutions'] = data['resolutions']
-    write(env.get_template('player.html').render(data=data, stream=stream), 'watch/' + stream['link'] + '.html')
+    write(tem('player').render(data=data, stream=stream), f'watch/{stream["link"]}')
 
+# Create and write liveset and individual video pages
 def liveset(sets):
     for set in sets:
-        write(env.get_template('liveset.html').render(data=data, set=set), 'sets/' + set['link'] + '.html')
-        for vid in set["videos"]:
-            vid["resolutions"] = []
-            vid["filetype"] = "mkv"
-            vid["poster"] = set["poster"]
-            name = vid["name"]
-            vid["name"] = name + ' @ ' + set["name"]
-            write(env.get_template('player.html').render(data=data, stream=vid), 'watch/' + set['link'] + '/' + name + '.html')
+        write(tem('liveset').render(data=data, set=set), f'sets/{set["link"]}')
+        for vid in set['videos']:
+            # Move some stuff around to make it fit with the player template
+            vid['resolutions'] = []
+            vid['filetype'] = 'mkv'
+            vid['poster'] = set['poster']
+            name = vid['name']
+            vid['name'] = name + ' @ ' + set['name']
+            write(tem('player').render(data=data, stream=vid), f'watch/{set["link"]}/{name}')
 liveset(data['livesets_menu'])
 liveset(data['livesets_cards'])
