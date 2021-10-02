a33f75766a7f4381011da0f37d3cc7443aab2d25
Ellie Ang <git@angm.xyz>
2021-10-02 21:20:18 +0200

    Stream viewer counters

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
 const elems = document.getElementsByClassName('stream-status');
 
-for (const elem of elems) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                elem.classList.add('stream-status-online');
-                elem.innerHTML = 'Online';
-            } else {
-                elem.classList.add('stream-status-offline');
-                elem.innerHTML = 'Offline';
+function refreshStreams() {
+    for (const elem of elems) {
+        makeRequest(elem.getAttribute("data-videourl"), function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    elem.classList.add('stream-status-online');
+                    setViewerCount(elem);
+                } else {
+                    elem.classList.add('stream-status-offline');
+                    elem.innerHTML = 'Offline';
+                }
             }
-        }
+        })
     };
-    xhttp.open("GET", elem.getAttribute("data-videourl"), true);
+}
+
+function setViewerCount(elem) {
+    const url = "https://viewers.bluecrazii.nl/views/" + elem.getAttribute("data-name");
+    makeRequest(url, function () {
+        if (this.readyState == 4 && this.status == 200) {
+            elem.innerHTML = this.responseText;
+        }
+    });
+}
+
+function makeRequest(url, callback) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = callback;
+    xhttp.open("GET", url, true);
     xhttp.send();
-};
+}
+
+refreshStreams();
+setInterval(() => refreshStreams(), 5000);
